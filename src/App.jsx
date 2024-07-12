7d33646d1295f243bd3867b782005019fbbb61ab
ekorahy <ekorahy@gmail.com>
2024-07-12 21:01:18 +0700

    style: add container style for main element

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import Home from "./pages/Home";
 export default function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <main className="container mx-auto p-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
     </div>
   );
 }
